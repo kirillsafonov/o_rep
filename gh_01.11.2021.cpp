ab928b10d7c06dde4aea3c33aa3929228e3365ce
quriny <st095270@student.spbu.ru>
2021-12-18 05:24:34 +0300

    Create gh_01.11.2021.cpp

---
+++ gh_01.11.2021.cpp
@@ -0,0 +1,23 @@
+﻿// g.h_01.11.2021.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+//Представление двумерного массива одномерным
+int main()
+{
+    const int n = 4, m = 5;
+    int A[n * m];
+    for (int i = 0;i<n;++i) {
+        for (int j = 0;j<m;++j) {
+            A[i * m + j] = i + j;
+        }
+    }
+    std::cout << "Matrix = " << std::endl;
+    for (int i = 0;i<n;++i) {
+        for (int j = 0;j<m;++j) {
+            std::cout << A[i * m + j]<<" ";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
