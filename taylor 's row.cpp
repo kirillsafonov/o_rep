594436b36f3cb9c89688741c8808494dafab822e
quriny <st095270@student.spbu.ru>
2021-12-18 08:47:02 +0300

    Create taylor 's row.cpp

---
+++ taylor 's row.cpp
@@ -0,0 +1,23 @@
+﻿// Taylor 's row.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+	long double x;
+	std::cout << "Input x: ";
+	std::cin >> x;
+	unsigned long long j = 2 * 3;
+	int n;
+	std::cout << "Input n: ";
+	std::cin >> n;
+	long double a = -((x * x * x) / (j));
+	for (int i = 2; i <= n + 1; ++i) {
+		x += a;
+		j *= (4 * i * i + 2 * i);
+		a *= (x * x) / j;
+		std::cout << j << std::endl;
+	}
+	std::cout << "sin(x)=" << x << std::endl;
+}
