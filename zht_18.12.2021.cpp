e98a3090a17e56caa43f14d5a632aa830b5a3ce7
quriny <st095270@student.spbu.ru>
2021-12-21 23:43:44 +0300

    Create zht_18.12.2021.cpp

---
+++ zht_18.12.2021.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+//Умножение матрицы на число
+int** multMatrix(int** A, int row, int column, int number) {
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < column; ++j) {
+            A[i][j] *= number;
+        }
+    }
+    return A;
+}
+
+//Перестановка строк с зависимыми элементами
+int exLin(int arr[], int rows, int cols, int a, int b) {
+    int tmp;
+    for (int j = 0; j < cols; ++j) {
+        tmp = arr[(a - 1) * cols + j];
+        arr[(a - 1) * cols + j] = arr[(b - 1) * cols + j];
+        arr[(b - 1) * cols + j] = tmp;
+    }
+    return 0;
+}
+
+//Вывод шестнадцетиричного числа заданного размера
+int main() {
+    srand(time(NULL));
+    int n;
+    std::cout << "Input n: ";
+    std::cin >> n;
+    char hx[16] = { '0', '1', '2', '3', '4', '5', '6','7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', };
+    char* hxn = new char[n];
+    for (int i = 0;i < n;++i) {
+        hxn[i] = hx[rand() % 16];
+        std::cout << hxn[i];
+    }
+    delete[]hxn;
+}
